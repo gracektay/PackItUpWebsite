009a1f21fce9f901cec0ebdd63c90870239f1fcf
gracektay <gktaydesigns@gmail.com>
2016-11-13 01:19:41 -0500

    Uploaded webpages and updated ReadMe

---
+++ gam465website3/js/notepad.js
@@ -0,0 +1,12 @@
+"use strict";
+$(document).ready(function(){
+  $('#title').focus();
+    $('#text').autosize();
+});// JavaScript Document
+
+
+
+$('document').ready(function() {
+  var msg = $('#message');
+  msg.autosize();
+});
